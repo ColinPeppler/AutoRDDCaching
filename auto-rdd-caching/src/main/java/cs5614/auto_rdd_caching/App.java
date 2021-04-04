8089ebce51ad2bbbfb7871ceb5775f91bfe22f37
Colin Peppler <colinpeppler@gmail.com>
2021-04-04 11:00:00 -0400

    Add maven project directory

---
+++ auto-rdd-caching/src/main/java/cs5614/auto_rdd_caching/App.java
@@ -0,0 +1,13 @@
+package cs5614.auto_rdd_caching;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
