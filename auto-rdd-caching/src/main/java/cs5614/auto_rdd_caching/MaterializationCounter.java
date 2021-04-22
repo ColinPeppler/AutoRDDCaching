4353a77c0554f5473ba46d64f89f985ec4264e5c
Arash Behpour <arashb97@vt.edu>
2021-04-22 18:03:49 -0400

    Added wrapper class, fixed job4, bestRDDtoPersist does not look at cached RDDs

---
+++ auto-rdd-caching/src/main/java/cs5614/auto_rdd_caching/MaterializationCounter.java
@@ -0,0 +1,156 @@
+package cs5614.auto_rdd_caching;
+
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDDLike;
+//import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.Function2;
+//import org.apache.spark.api.java.function.VoidFunction;
+import org.apache.spark.rdd.RDD;
+
+/**
+ * This class performs the materialization actions for an RDD (Ex. collect())
+ *  in Apache Spark and records them. 
+ */
+public class MaterializationCounter {
+    
+    HashMap<RDD<?>, Integer> actionRDDs;
+    
+    /**
+     * Helper method for all action methods, to put an Action RDD into HashMap
+     * @param javaActionRDD Any type of JavaRDDLike object
+     */
+    private void putRDDInMap(JavaRDDLike<?, ?> javaActionRDD) {
+        RDD<?> actionRDD = javaActionRDD.rdd();
+        // If RDD is already cached then we do not want to consider it.
+        if(actionRDD.getStorageLevel().useMemory() == true)
+            return;
+        
+        if (actionRDDs.containsKey(actionRDD)) {
+            Integer prevValue = actionRDDs.get(actionRDD);
+            actionRDDs.replace(actionRDD, prevValue + 1);
+        }
+        else {
+            actionRDDs.put(actionRDD, 1);
+        }
+    }
+    
+    /**
+     * Constructs a MaterlizationCounter object
+     */
+    public MaterializationCounter() {
+        actionRDDs = new HashMap<RDD<?>, Integer>();
+    }
+    
+    /**
+     * Mimics the Apache Spark collect() function while recording the 
+     * RDD used for materialization
+     * @param javaActionRDD an Java RDD object (Ex. JavaPairRDD, JavaRDD)
+     * @return a representation of the RDD as a list
+     */
+    public List<?> collect(JavaRDDLike<?, ?> javaActionRDD){
+        this.putRDDInMap(javaActionRDD);
+        return javaActionRDD.collect(); 
+    }
+    
+    /**
+     * Mimics the Apache Spark count() function while recording the RDD used for materialization
+     * @param javaActionRDD an Java RDD object (Ex. JavaPairRDD, JavaRDD)
+     * @return a long value that represents the number of entries in the RDD
+     */
+    public Long count(JavaRDDLike<?, ?> javaActionRDD) {
+        this.putRDDInMap(javaActionRDD);
+        return javaActionRDD.count();
+    }
+    
+    public Object first(JavaRDDLike<?, ?> javaActionRDD){
+        this.putRDDInMap(javaActionRDD);
+        return javaActionRDD.first();
+    }
+    
+    public List<?> take(JavaRDDLike<?, ?> javaActionRDD, int n){
+        this.putRDDInMap(javaActionRDD);
+        return javaActionRDD.take(n);
+    }
+    
+    public Object reduce(JavaRDDLike<Object, ?> javaActionRDD, Function2<Object, Object, Object> func){
+        this.putRDDInMap(javaActionRDD);
+        return javaActionRDD.reduce(func);
+    }
+    
+    public List<?> takeSample(JavaRDDLike<?, ?> javaActionRDD, 
+        boolean withReplacement, int num){
+        this.putRDDInMap(javaActionRDD);
+        return javaActionRDD.takeSample(withReplacement, num);
+    }
+    
+    public List<?> takeSample(JavaRDDLike<?, ?> javaActionRDD, 
+        boolean withReplacement, int num, long seed){
+        this.putRDDInMap(javaActionRDD);
+        return javaActionRDD.takeSample(withReplacement, num, seed);
+    }
+    
+    public List<?> takeOrdered(JavaRDDLike<?, ?> javaActionRDD, int num){
+        this.putRDDInMap(javaActionRDD);
+        return javaActionRDD.takeOrdered(num);
+    }
+    
+    public List<?> takeOrdered(JavaRDDLike<Object, ?> javaActionRDD, int num, Comparator<Object> comp){
+        this.putRDDInMap(javaActionRDD);
+        return javaActionRDD.takeOrdered(num, comp);
+    }
+    
+    public void saveAsTextFile(JavaRDDLike<?, ?> javaActionRDD, String path){
+        this.putRDDInMap(javaActionRDD);
+        javaActionRDD.saveAsTextFile(path);
+    }
+    
+    public void saveAsTextFile(JavaRDDLike<?, ?> javaActionRDD, String path,
+        Class<? extends org.apache.hadoop.io.compress.CompressionCodec> codec){
+        this.putRDDInMap(javaActionRDD);
+        javaActionRDD.saveAsTextFile(path, codec);
+    }
+    
+    public void saveAsObjectFile(JavaRDDLike<?, ?> javaActionRDD, String path){
+        this.putRDDInMap(javaActionRDD);
+        javaActionRDD.saveAsObjectFile(path);
+    }
+    
+    public Map<?, Long> countByKey(JavaPairRDD<?, ?> JavaPairActionRDD){
+        this.putRDDInMap(JavaPairActionRDD);
+        return JavaPairActionRDD.countByKey();
+    }
+    
+    /*// Both functions do not work
+    public void saveAsHadoopFile(JavaPairRDD<?, ?> JavaPairActionRDD,
+        String path,
+        Class<?> keyClass,
+        Class<?> valueClass,
+        Class<Object> outputFormatClass){
+        this.putRDDInMap(JavaPairActionRDD);
+        JavaPairActionRDD.saveAsHadoopFile(path, keyClass, valueClass, outputFormatClass);
+    }
+    public void foreach(JavaPairRDD<?, ?> JavaPairActionRDD, VoidFunction<?> f){
+        this.putRDDInMap(JavaPairActionRDD);
+        JavaPairActionRDD.foreach(f);
+    }
+    */
+    
+    /**
+     * Gets the RDDs that had previous actions called upon them for materialization
+     * @return a HashMap of RDDs to the number of times it had been materialized
+     */
+    public HashMap<RDD<?>, Integer> getActionRDDs(){
+        return actionRDDs;
+    }
+    
+    /**
+     * Clears the Hashmap of RDDs that had previous materialized actions called.
+     */
+    public void clearActionRDDs() {
+        actionRDDs.clear();
+    }
+}
--- auto-rdd-caching/src/main/java/cs5614/auto_rdd_caching/OurSimpleApp.java
+++ auto-rdd-caching/src/main/java/cs5614/auto_rdd_caching/OurSimpleApp.java
@@ -10,47 +10,60 @@ import java.util.Map;
  * run `mvn package`, then submit jar file to spark
  */
 public class OurSimpleApp {
+    private static MaterializationCounter c = new MaterializationCounter();
+    
     public static void main( String[] args ) {
         System.out.println("##### The Beginning #####");
-        System.setProperty("hadoop.home.dir", "C:\\spark-3.1.1-bin-hadoop2.7"); // Arash's filePath
+        //System.setProperty("hadoop.home.dir", "C:\\spark-3.1.1-bin-hadoop2.7"); // Arash's filePath
 
         SparkConf conf = new SparkConf()
             .setAppName("Our Simple App")
             .setMaster("local[4]"); // runs on 4 worker threads
         JavaSparkContext sc = new JavaSparkContext(conf);
         sc.setLogLevel("ERROR");
-
-        Map<RDD<?>, Integer> actionRDDs1 = ExampleSparkJobs.job1(sc, true);
-        DAG dag1 = new DAG(actionRDDs1);
+        
+        ExampleSparkJobs.job1(sc, c, true);
+        DAG dag1 = new DAG(c.getActionRDDs());
         System.out.println("DAG: " + dag1);
         System.out.println("---");
         System.out.println(dag1.toLocationsString());
         System.out.println(dag1.toRDDIdentifierString());
         System.out.println(dag1.toString());
-        Map<RDD<?>, Integer> actionRDDs2 = ExampleSparkJobs.job2(sc, true);
-        DAG dag2 = new DAG(actionRDDs2);
+        c.clearActionRDDs();
+        
+        ExampleSparkJobs.job2(sc, c, true);
+        DAG dag2 = new DAG(c.getActionRDDs());
         System.out.println("DAG: " + dag2);
         System.out.println(dag2.toLocationsString());
-        Map<RDD<?>, Integer> actionRDDs3 = ExampleSparkJobs.job3(sc, true);
-        DAG dag3 = new DAG(actionRDDs3);
+        c.clearActionRDDs(); 
+        
+        ExampleSparkJobs.job3(sc, c, true);
+        DAG dag3 = new DAG(c.getActionRDDs());
         dag3.sortDAGByID();
         System.out.println("DAG: " + dag3);
         System.out.println(dag3.toLocationsString());
         System.out.println(dag3.toRDDIdentifierString());
         System.out.println(dag3.toStringWithAttributes());
-        Map<RDD<?>, Integer> actionRDDs4 = ExampleSparkJobs.job4(sc, true);
-        DAG dag4 = new DAG(actionRDDs4);
+        c.clearActionRDDs();
+        
+        ExampleSparkJobs.job4(sc, c, true); 
+        DAG dag4 = new DAG(c.getActionRDDs());
         System.out.println("DAG: " + dag4);
         System.out.println(dag4.toLocationsString());
         System.out.println(dag4.toRDDIdentifierString());
-        Map<RDD<?>, Integer> actionRDDs5 = ExampleSparkJobs.job5(sc, true);
-        DAG dag5 = new DAG(actionRDDs5);
+        c.clearActionRDDs();
+        
+        ExampleSparkJobs.job5(sc, c, true);
+        DAG dag5 = new DAG(c.getActionRDDs());
         System.out.println("DAG: " + dag5);
         System.out.println(dag5.toLocationsString());
-        Map<RDD<?>, Integer> actionRDDs6 = ExampleSparkJobs.job6(sc, true);
-        DAG dag6 = new DAG(actionRDDs6);
+        c.clearActionRDDs();
+        
+        ExampleSparkJobs.job6(sc, c, true);
+        DAG dag6 = new DAG(c.getActionRDDs());
         System.out.println("DAG: " + dag6);
-
+        c.clearActionRDDs();
+        
         System.out.println("Best RDDs To Persist:");
         System.out.printf("Job 1: %s%n", dag1.getRDDToPersist());
         System.out.printf("Job 2: %s%n", dag2.getRDDToPersist());
@@ -58,11 +71,11 @@ public class OurSimpleApp {
         System.out.printf("Job 4: %s%n", dag4.getRDDToPersist());
         System.out.printf("Job 5: %s%n", dag5.getRDDToPersist());
         System.out.printf("Job 6: %s%n", dag6.getRDDToPersist());
-
+        
         int n_times = 20;
-        long time = ExampleSparkJobs.timeNCalls(sc, n_times);
+        long time = ExampleSparkJobs.timeNCalls(sc, c, n_times);
         System.out.printf("%d executions of job4 takes %.2f seconds%n", n_times, time/1000000000.0);
-
+     
         System.out.println("##### The End #####");
     }
 }
